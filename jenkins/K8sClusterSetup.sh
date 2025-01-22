cb5517171e1839ec6a8849d6eae0472125e9290f
AntoineC <antoinecraipeau@outlook.fr>
2025-01-22 16:41:45 +0100

    Documentation et deployment fix

--- jenkins/K8sClusterSetup.sh
+++ jenkins/K8sClusterSetup.sh
@@ -13,4 +13,9 @@ kubectl create token jenkins --duration=8760h
 # Copy the token and add it as a Secret Text credential in Jenkins "minikube-jenkins-token"
 # See https://phati-sawant.medium.com/connect-to-a-kubernetes-cluster-and-execute-kubectl-commands-in-jenkins-pipeline-a3f474c1302f
 
+# Dans le folder webapi-k8s
+
+helm upgrade --install api-bcff -n dev helm_dev
 kubectl port-forward service/api-bcff 9988:8080 -n dev --address 0.0.0.0
+
+# Cela permet d'ouvrir le service sur le port 9988 de la machine hôte pour qu'il soit accessible depuis Jenkins pour l'étape de test
